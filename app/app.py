6a0f120370a4fe88e863250c2ef2a788984df284
Carlos Quintanilha <cquinta@gmail.com>
2024-08-20 21:57:00 -0300

    env secrets e configs

--- app/app.py
+++ app/app.py
@@ -12,5 +12,7 @@ async def variaveis():
     variaveis = dict()
     variaveis['version'] = os.environ.get('APP_VERSION')
     variaveis['token']=os.environ.get('TOKEN')
+    variaveis['bancodev']=os.environ.get('BANCODEV')
+    variaveis['CONFIG'] = open('/app/config-dev.yaml', "r").read()
     return {"variaveis": variaveis}
 
