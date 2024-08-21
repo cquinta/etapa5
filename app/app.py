bbf10e8ee7683e36dc4b420ee98e41d5eb1bef0e
Carlos Quintanilha <cquinta@gmail.com>
2024-08-20 21:41:42 -0300

    first compose

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 app = FastAPI()
 
@@ -5,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello Carlos"}
+
+@app.get("/variaveis")
+async def variaveis():
+    variaveis = dict()
+    variaveis['version'] = os.environ.get('APP_VERSION')
+    variaveis['token']=os.environ.get('TOKEN')
+    return {"variaveis": variaveis}
+
